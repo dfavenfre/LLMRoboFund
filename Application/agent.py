0d1b2b60b58a06762bca02ae52440fd2a9e54cea
Tolga <dfavenfre@protonmail.com>
2023-12-18 01:06:13 +0300

    Update agent.py

--- Application/agent.py
+++ Application/agent.py
@@ -17,7 +17,7 @@ def run_robofund_agent(query: str) -> str:
     script_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_dir, "intermediate_agent_template.json")
     system_prompt = ""
-    with open("intermediate_agent_template.json", "r") as file:
+    with open(file_path, "r") as file:
         system_prompt = json.load(file)
     llm = ChatOpenAI(
         openai_api_key=st.secrets.OPENAI_API_KEY,
